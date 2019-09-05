a173f3148572f1e1f8ff2e58c6f91801ff6a3e94
Rodrigo Felipe <rodrigogfelipe@hotmail.com>
2019-09-05 16:57:58 -0300

    findById implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,11 +1,25 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao {
+
+	private Connection conn;
+
+	public SellerDaoJDBC(Connection conn) {
+		this.conn = conn;
+
+	}
 
 	@Override
 	public void insert(Seller obj) {
@@ -27,8 +41,57 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public Seller findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+
+		PreparedStatement st = null;
+		ResultSet rs = null;
+
+		try {
+			// Comando executar no BD
+			st = conn.prepareStatement(
+
+					"SELECT seller.*,department.Name as DepName "
+
+							+ "FROM seller INNER JOIN department "
+
+							+ "ON seller.DepartmentId = department.Id "
+
+							+ "WHERE seller.Id = ?");
+			// ? e igual 1 recebendo o ID de valor
+			st.setInt(1, id);
+			// executeQuery() recebera o resultado do BD
+			rs = st.executeQuery();
+			// if rs.next ocndicaõa de verificacão se a registro no BD, caso não estiver
+			// retornara NULL . Caso contrario retorna a tabela BD
+			if (rs.next()) {
+
+				Department dep = instantiateDepartment(rs);
+
+				Seller obj = instantiateSeller(rs, dep);
+
+				return obj;
+
+			}
+
+			return null;
+
+		}
+
+		catch (SQLException e) {
+
+			throw new DbException(e.getMessage());
+
+		}
+
+		finally {
+
+			DB.closeStatement(st);
+
+			DB.closeResultSet(rs);
+
+		}
+
+	}
+
 	}
 
 	@Override
